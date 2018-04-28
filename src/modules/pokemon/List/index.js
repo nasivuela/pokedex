bac685e5662bc2f1514b3d0ee95a5e9eacb4c2ae
Nasiba Carrasco <nasivuela@gmail.com>
2018-04-28 20:27:19 +0200

    Make List component reactive

--- src/modules/pokemon/List/index.js
+++ src/modules/pokemon/List/index.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { observer }  from 'mobx-react';
 
 class PokemonList extends Component {
-  render() {
-    return <div>
-      List
-      <Link to="/detail">
-        To detail
-      </Link>
-    </div>;
-  }
-}
 
-export default PokemonList;
+  render() {
+    const { store } = this.props;
+    const pokemons = store.pokemons;
+
+    if(!pokemons.length) return 'LOADING';
+
+    return (
+      <div>
+        <Link to="/detail">
+          To detail
+        </Link>
+        <div>
+          {pokemons
+            .map(pokemon => (
+              <div key={pokemon.id}>
+                {pokemon.id}
+                {pokemon.name}
+              </div>
+            ))
+          }
+        </div>
+      </div>
+    )
+  }
+};
+
+export default observer(PokemonList);
