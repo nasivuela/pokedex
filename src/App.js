ef8e5008778abc5d061416ac146f9c420412ee80
Nasiba Carrasco <nasivuela@gmail.com>
2018-04-28 18:53:53 +0200

    Rename as Pokedex

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import styles from './App.scss';
 
 class App extends Component {
@@ -7,12 +6,8 @@ class App extends Component {
     return (
       <div className={styles.app}>
         <header className={styles.appHeader}>
-          <img src={logo} className={styles.appLogo} alt="logo" />
-          <h1 className={styles.appTitle}>Welcome to React</h1>
+          <h1 className={styles.appTitle}>Pokedex</h1>
         </header>
-        <p className={styles.appIntro}>
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
