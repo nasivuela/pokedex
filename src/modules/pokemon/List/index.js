6d70a3e790a0553c6b8621111d6f0cd38763fe69
Nasiba Carrasco <nasivuela@gmail.com>
2018-04-28 20:51:51 +0200

    Add InputSearch to PokemonList

--- src/modules/pokemon/List/index.js
+++ src/modules/pokemon/List/index.js
@@ -5,14 +5,39 @@ import {
   observer,
   PropTypes as MobxPropTypes
 } from 'mobx-react';
+import InputSearch from 'modules/common/InputSearch';
 import styles from './styles.scss';
 
 class PokemonList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      search: '',
+    };
+
+    this.handleSearch = this.handleSearch.bind(this);
+  }
+
   componentDidMount() {
     const { store } = this.props;
     store.fetchPokemons();
   }
+
+  handleSearch(e) {
+    this.setState({search: e.target.value});
+  }
+
+  filteredPokemons() {
+    const { search } = this.state;
+    const { store } = this.props;
+    return search
+      ? store.pokemons
+        .filter(pokemon => pokemon.name.includes(search))
+      : store.pokemons;
+  }
+
   render() {
+    const {search} = this.state;
     const { store } = this.props;
     const pokemons = store.pokemons;
 
@@ -23,8 +48,13 @@ class PokemonList extends Component {
         <Link to="/detail">
           To detail
         </Link>
+        <InputSearch
+          onChange={this.handleSearch}
+          value={search}
+          placeholder="Filtra pokemons por nombre"
+        />
         <div className={styles.list}>
-          {pokemons
+          {this.filteredPokemons()
             .map(pokemon => (
               <div key={pokemon.id}>
                 {pokemon.id}
